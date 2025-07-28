2516aa9eae0a9d5cccfbf85391215d87acc9b892
Icbitic <79243906+Icbitic@users.noreply.github.com>
2025-07-29 01:02:02 +0800

    Add example standard solution

---
+++ std.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    // Standard solution - simple addition
+    cout << a + b << endl;
+    
+    return 0;
+}
